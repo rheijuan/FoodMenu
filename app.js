e0288172be013ebe3da3d65924c9c134ef28c37f
Wesley Kayanan <wesleykayananwork@gmail.com>
2021-10-17 17:13:08 +0800

    Simplify project form

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.get('/projects', (req, res) => {
 
 // Add Page
 app.get('/projects/add', (req, res) => {
-    res.render('project/add')
+    res.render('project/project-form')
 })
 
 // Details Page
@@ -96,7 +96,7 @@ app.get('/projects/:id', (req, res) => {
 app.get('/projects/update/:id', (req, res) => {
     const id = req.params.id
     Project.findById(id).then(result => {
-        res.render('project/update', {project: result})
+        res.render('project/project-form', {project: result})
     }).catch(err => {
         console.log(err)
     })
